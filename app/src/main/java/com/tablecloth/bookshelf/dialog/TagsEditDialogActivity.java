ac95dc8597e45344b4059749ce9e5b8e9bc92544
table-cloth <table.cloth.03@gmail.com>
2015-04-08 09:05:10 +0900

    タグ履歴機能作成中。

--- app/src/main/java/com/tablecloth/bookshelf/dialog/TagsEditDialogActivity.java
+++ app/src/main/java/com/tablecloth/bookshelf/dialog/TagsEditDialogActivity.java
@@ -3,9 +3,14 @@ package com.tablecloth.bookshelf.dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.tablecloth.bookshelf.R;
@@ -28,6 +33,7 @@ public class TagsEditDialogActivity extends DialogBaseActivity {
     ViewGroup recentTagContainer;
     String tagsData;
     EditText addTagEditText;
+//    ViewGroup recentTagTmpContainer;
 
 
     @Override
@@ -51,6 +57,8 @@ public class TagsEditDialogActivity extends DialogBaseActivity {
                 finish();
             }
         });
+
+        // テキスト入力によるタグ追加
         addTagEditText = (EditText)findViewById(R.id.data_content);
         findViewById(R.id.btn_add_tag).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,13 +94,15 @@ public class TagsEditDialogActivity extends DialogBaseActivity {
         ArrayList<String> tags = FilterDao.getTagsData(tagsData);
         tagContainer.removeAllViews();
         recentTagContainer.removeAllViews();
+
         ViewUtil.setTagInfoNormal(TagsEditDialogActivity.this, tags, tagContainer);
         tagContainer.invalidate();
 
         ArrayList<String> tagsLog = FilterDao.loadTags(TagsEditDialogActivity.this);
 
-        recentTagContainer = ViewUtil.setTagInfoNormal(TagsEditDialogActivity.this, tagsLog, tagContainer);
-        recentTagContainer.invalidate();
+//        if(recentTagTmpContainer != null) recentTagTmpContainer.removeAllViews();
+//        recentTagTmpContainer = ViewUtil.setTagInfoNormal(TagsEditDialogActivity.this, tagsLog, recentTagContainer);
+//        reInvalidateRecentTags();
     }
 
     @Override
@@ -109,4 +119,100 @@ public class TagsEditDialogActivity extends DialogBaseActivity {
 
         return intent;
     }
+
+    /**
+     * タグ履歴のレイアウト再配置
+     * @param hasFocus
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+//        if(recentTagContainer == null || recentTagTmpContainer == null) {
+//            ToastUtil.show(TagsEditDialogActivity.this, "エラー発生");
+//        }
+
+//        reInvalidateRecentTags();
+    }
+
+//    private void reInvalidateRecentTags() {
+//
+////        if(recentTagContainer == null || recentTagTmpContainer == null) return;
+//
+//        // 履歴領域の情報を取得
+//        int maxWidth = recentTagTmpContainer.getWidth();
+//        int maxChild = recentTagTmpContainer.getChildCount();
+//        int lineStartIndex = 0;
+//        int lineStartViewId = -1;
+//        if(maxChild > 0) lineStartViewId = recentTagTmpContainer.getChildAt(0).getId();
+//        int currentWidth = 0;
+//        int prevId = -1;
+//
+////        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+////        LinearLayout currentRow = (LinearLayout)inflater.inflate(R.layout.tag_row_layout, null);
+////
+////        recentTagContainer.removeAllViews();
+////        recentTagContainer.addView(currentRow);
+//
+//        for(int i = 0 ; i < maxChild ; i ++) {
+//            ViewGroup tagChild = null;
+//            try {
+//                tagChild = (ViewGroup)recentTagTmpContainer.getChildAt(i);
+//                // タップによるタグの登録処理
+//                tagChild.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        String newTag = ((TextView)view.findViewById(R.id.tag_name)).getText().toString();
+//                        ArrayList<String> tagsTmp = FilterDao.getTagsData(tagsData);
+//
+//                        // 登録失敗
+//                        if(Util.isEmpty(newTag)) {
+//                            ToastUtil.show(TagsEditDialogActivity.this, "追加するタグを入力してください");
+//                            return;
+//                        } else if(tagsTmp != null && tagsTmp.contains(newTag)) {
+//                            ToastUtil.show(TagsEditDialogActivity.this, "既に登録済みのタグです");
+//                            return;
+//                        }
+//
+//                        // 登録成功
+//                        if(tagsTmp == null) tagsTmp = new ArrayList<String>();
+//                        tagsTmp.add(newTag);
+//                        tagsData = FilterDao.getTagsStr(tagsTmp);
+//                        updateTags();
+//                        FilterDao.saveTags(TagsEditDialogActivity.this, newTag);
+//                    }
+//                });
+//
+//
+//                // ビューの設定
+//                currentWidth += tagChild.getWidth();
+//                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)tagChild.getLayoutParams();
+//                // その行の１つ目 or 横幅に余裕がある場合
+//                if(lineStartIndex == i || currentWidth < maxWidth) {
+//                    if(prevId != -1) {
+//                        params.addRule(RelativeLayout.ALIGN_TOP, prevId);
+//                        params.addRule(RelativeLayout.RIGHT_OF, prevId);
+//                    }
+//                } else {
+//                    if(prevId != -1) {
+//                        params.addRule(RelativeLayout.BELOW, lineStartViewId);
+//                        lineStartViewId = tagChild.getId();
+//                        lineStartIndex = i;
+//                        currentWidth = 0;
+//                        currentRow.invalidate();
+//                        currentRow = (LinearLayout)inflater.inflate(R.layout.tag_row_layout, null);
+//                        recentTagContainer.addView(currentRow);
+//                    }
+//                }
+//                currentRow.addView(tagChild);
+//            } catch (OutOfMemoryError e) {
+//                e.printStackTrace();
+//            }
+//            if(tagChild != null) {
+//                prevId = tagChild.getId();
+//            }
+//        }
+//
+//        recentTagContainer.invalidate();
+//    }
 }
